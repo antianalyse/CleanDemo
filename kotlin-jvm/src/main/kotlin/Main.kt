87755331e758554110ec462af8c8263282ee690f
程守一 <2469758101@qq.com>
2022-08-30 15:15:34 +0800

    重构

--- kotlin-jvm/src/main/kotlin/Main.kt
+++ kotlin-jvm/src/main/kotlin/Main.kt
@@ -1,13 +1,10 @@
-
-import jdk.nashorn.api.scripting.ScriptObjectMirror
 import java.util.concurrent.Executors
 
 fun main() {
 
     Executors.newSingleThreadExecutor().submit {
-      println(222)
-   }
-
+        println(222)
+    }
 
 
 }
